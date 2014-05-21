444cd30d1485839dbc0d91732d11f93a3ea0c251
Matt Jenkins <matt@majenko.co.uk>
2014-05-21 12:36:24 +0100

    Fixed backlight setting

--- examples/PICadillo-35T/PICadilloTest/PICadilloTest.ino
+++ examples/PICadillo-35T/PICadilloTest/PICadilloTest.ino
@@ -7,8 +7,7 @@ AnalogTouch ts(LCD_XL, LCD_XR, LCD_YU, LCD_YD, 320, 480);
 
 const int btnSize = 64;
 
-//const int backlight = PIN_BACKLIGHT;
-const int backlight = 13;
+const int backlight = PIN_BACKLIGHT;
 
 uint16_t colors[8] = {
 	Color::Black,
